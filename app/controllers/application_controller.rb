04acc0eca0e20b936651f1a82223d0147b8b4065
Constantin Hetagurov <koshtt@gmail.com>
2012-05-15 15:46:11 +0400

    hmm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 	before_filter :message
 	def message
-		$sometext = params['sometext']
+		@sometext = params['sometext']
 	end
 end
